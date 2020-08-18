54ca5561af450c8c5957d2d27dfdfdb680f50c15
Ваше Имя <Antoniozhezhel@gmail.com>
2020-08-18 23:35:43 +0300

    substitute gender input to choose RadioButton

--- app/src/main/java/com/example/listofemployeessqlite/view/EmployeesLayoutActivity.java
+++ app/src/main/java/com/example/listofemployeessqlite/view/EmployeesLayoutActivity.java
@@ -9,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.example.listofemployeessqlite.R;
 import com.example.listofemployeessqlite.persistence.EmployeesModel;
@@ -18,13 +21,37 @@ import javax.inject.Inject;
 
 public class EmployeesLayoutActivity extends AppCompatActivity {
 
-
+    Intent data = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_emloyees_layout);
         addNew();
+
+        RadioGroup radioGroupGender = (RadioGroup) findViewById(R.id.radioGroupGenderq);
+        radioGroupGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButtonMale:
+                        data.putExtra("gender","Male");
+
+                        break;
+                    case R.id.radioButtonFemale:
+                        data.putExtra("gender","Female");
+                        break;
+
+                    case R.id.radioButtonOthers:
+                        data.putExtra("gender","Other");
+                        break;
+                }
+                setResult(RESULT_OK, data);
+
+            }
+        });
+
     }
 
     private void addNew() {
@@ -32,23 +59,23 @@ public class EmployeesLayoutActivity extends AppCompatActivity {
         Button addEmployees=findViewById(R.id.buttonAddq);
         EditText editTextEnterName=findViewById(R.id.editTextEnterNameq);
         EditText editTextEnterAge=findViewById(R.id.editTextEnterAgeq);
-        EditText editTextEnterGender=findViewById(R.id.editTextEnterGenderq);
+        //EditText editTextEnterGender=findViewById(R.id.editTextEnterGenderq);
         EditText editTextEnterPhone=findViewById(R.id.editTextEnterPhoneq);
 
         addEmployees.setOnClickListener(v -> {
 
+
+
+
             if (!TextUtils.isEmpty(editTextEnterName.getText()) && !TextUtils.isEmpty(editTextEnterAge.getText())
-                    && !TextUtils.isEmpty(editTextEnterGender.getText()) && !TextUtils.isEmpty(editTextEnterPhone.getText())) {
+                    && !TextUtils.isEmpty(editTextEnterPhone.getText())) {
 
                 String name = editTextEnterName.getText().toString();
                 int age = Integer.parseInt(editTextEnterAge.getText().toString());
-                String gender = editTextEnterGender.getText().toString();
                 long phone = Long.parseLong(editTextEnterPhone.getText().toString());
 
-                Intent data = new Intent();
                 data.putExtra("name",name);
                 data.putExtra("age",age);
-                data.putExtra("gender",gender);
                 data.putExtra("phone",phone);
 
                 setResult(RESULT_OK, data);
